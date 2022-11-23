2a09291847ef913435936aba99599ddb82512c10
surpmh <cdd8816@gmail.com>
2022-11-23 20:03:44 +0900

    기능개발 해결

---
+++ 기능개발.py
@@ -0,0 +1,27 @@
+from collections import deque
+import math
+
+def solution(progresses, speeds):
+    q = deque()
+    answer = []
+    
+    for progress, speed in zip(progresses, speeds):
+        q.append(math.ceil((100 - progress) / speed))
+        
+    d = q[0]
+    q.popleft()
+    count = 1
+
+    while q:
+        print(q, d)
+        if q[0] <= d:
+            count += 1
+            q.popleft()
+        else:
+            answer.append(count)
+            count = 0
+            d = q[0]
+
+    answer.append(count)
+
+    return answer
