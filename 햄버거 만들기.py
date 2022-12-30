0ebf4ef6fe5c35a9b80a3597f343761fbe270161
surpmh <cdd8816@gmail.com>
2022-12-30 21:15:29 +0900

    햄버거 만들기 해결

---
+++ 햄버거 만들기.py
@@ -0,0 +1,13 @@
+def solution(ingredient):
+    answer = 0
+    stack = []
+
+    for i in ingredient:
+        stack.append(i)
+
+        if len(stack) >= 4:
+            if stack[-1:-5:-1] == [1, 3, 2, 1]:
+                del stack[-1:-5:-1]
+                answer += 1
+
+    return answer
